c58d1336806548b303d7cb185c82173270618c01
vertesip <vertesip@gain.uni-sopron.hu>
2021-08-08 18:56:27 +0200

    Just some formatting

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -27,240 +27,244 @@ int main() {
 	cout << "Press enter to start the game (if it does not start, press enter a couple more times)" << endl;
 	cout << "Player 1 movement: W for up, S for down" << endl;
 	cout << "Player 2 movement: O for up, L for down" << endl;
+	cout << endl;
+	cout << "By: Patrik Vertesi" << endl;
 
 	if (_getch() == 13) {
 
-	while (true) {
+		while (true) {
 
-	// If the game ends, reset everything
+			// If the game ends, reset everything
 
-		if (gameEnd) {
-			ballX = 10;
-			ballY = 5;
-			gameEnd = false;
-		}
-
-	// Clear screen
-		system("CLS");
-
-	//Player movement
-		if (_kbhit()) {
-
-			if (GetAsyncKeyState('S') & 0x8000f) {
-				player1StartPoint = player1StartPoint++;
-				player1EndPoint = player1EndPoint++;
-			}
-			else if (GetAsyncKeyState('W') & 0x8000f) {
-				player1StartPoint = player1StartPoint--;
-				player1EndPoint = player1EndPoint--;
+			if (gameEnd) {
+				ballX = 10;
+				ballY = 5;
+				gameEnd = false;
 			}
 
-			if (GetAsyncKeyState('L') & 0x8000f) {
-				player2StartPoint = player2StartPoint++;
-				player2EndPoint = player2EndPoint++;
-			}
-			else if (GetAsyncKeyState('O') & 0x8000f) {
-				player2StartPoint = player2StartPoint--;
-				player2EndPoint = player2EndPoint--;
-			}
-		}
+			// Clear screen
+			system("CLS");
 
-		//Main loop for drawing the game
+			//Player movement
+			if (_kbhit()) {
 
-		for (size_t i = 0; i < 25; i++)
-		{
+				if (GetAsyncKeyState('S') & 0x8000f) {
+					player1StartPoint = player1StartPoint++;
+					player1EndPoint = player1EndPoint++;
+				}
+				else if (GetAsyncKeyState('W') & 0x8000f) {
+					player1StartPoint = player1StartPoint--;
+					player1EndPoint = player1EndPoint--;
+				}
 
-			//Draw player one
-			if (i >= player1StartPoint && i < player1EndPoint) {
-				cout << "|";
+				if (GetAsyncKeyState('L') & 0x8000f) {
+					player2StartPoint = player2StartPoint++;
+					player2EndPoint = player2EndPoint++;
+				}
+				else if (GetAsyncKeyState('O') & 0x8000f) {
+					player2StartPoint = player2StartPoint--;
+					player2EndPoint = player2EndPoint--;
+				}
 			}
-			else {
-				cout << " ";
-			}
 
-			//Draw the ball at the correct X position
-			if (i == ballY)
+			//Main loop for drawing the game
+
+			for (size_t i = 0; i < 25; i++)
 			{
-				switch (ballX)
+
+				//Draw player one
+				if (i >= player1StartPoint && i < player1EndPoint) {
+					cout << "|";
+				}
+				else {
+					cout << " ";
+				}
+
+				//Draw the ball at the correct X position
+				if (i == ballY)
 				{
-				case 1:
-					cout << "*                   ";
-					break;
-				case 2:
-					cout << " *                  ";
-					break;
-				case 3:
-					cout << "  *                 ";
-					break;
-				case 4:
-					cout << "   *                ";
-					break;
-				case 5:
-					cout << "    *               ";
-					break;
-				case 6:
-					cout << "     *              ";
-					break;
-				case 7:
-					cout << "      *             ";
-					break;
-				case 8:
-					cout << "       *            ";
-					break;
-				case 9:
-					cout << "        *           ";
-					break;
-				case 10:
-					cout << "         *          ";
-					break;
-				case 11:
-					cout << "          *         ";
-					break;
-				case 12:
-					cout << "           *        ";
-					break;
-				case 13:
-					cout << "            *       ";
-					break;
-				case 14:
-					cout << "             *      ";
-					break;
-				case 15:
-					cout << "              *     ";
-					break;
-				case 16:
-					cout << "               *    ";
-					break;
-				case 17:
-					cout << "                *   ";
-					break;
-				case 18:
-					cout << "                 *  ";
-					break;
-				case 19:
-					cout << "                  * ";
-					break;
-				case 20:
-					cout << "                   *";
-					break;
-				default:
+					switch (ballX)
+					{
+					case 1:
+						cout << "*                   ";
+						break;
+					case 2:
+						cout << " *                  ";
+						break;
+					case 3:
+						cout << "  *                 ";
+						break;
+					case 4:
+						cout << "   *                ";
+						break;
+					case 5:
+						cout << "    *               ";
+						break;
+					case 6:
+						cout << "     *              ";
+						break;
+					case 7:
+						cout << "      *             ";
+						break;
+					case 8:
+						cout << "       *            ";
+						break;
+					case 9:
+						cout << "        *           ";
+						break;
+					case 10:
+						cout << "         *          ";
+						break;
+					case 11:
+						cout << "          *         ";
+						break;
+					case 12:
+						cout << "           *        ";
+						break;
+					case 13:
+						cout << "            *       ";
+						break;
+					case 14:
+						cout << "             *      ";
+						break;
+					case 15:
+						cout << "              *     ";
+						break;
+					case 16:
+						cout << "               *    ";
+						break;
+					case 17:
+						cout << "                *   ";
+						break;
+					case 18:
+						cout << "                 *  ";
+						break;
+					case 19:
+						cout << "                  * ";
+						break;
+					case 20:
+						cout << "                   *";
+						break;
+					default:
+						cout << "                    ";
+						break;
+					}
+
+					//If the ball hits player one the boolean is set to true to increment the value later
+					if (ballX == 1) {
+						player1Shot = true;
+						player2Shot = false;
+					}
+					else if (!(i >= player1StartPoint && i < player1EndPoint) && ballX == 2) {
+						//If the ball is not hit by the player, the game ends
+						player2Points++;
+						system("CLS");
+						cout << "Player 2 wins" << endl;
+						for (size_t j = 0; j < 7; j++)
+						{
+							cout << "                    " << endl;
+						}
+						gameEnd = true;
+						system("pause>0");
+					}
+					if (ballX == 20) {
+						player1Shot = false;
+						player2Shot = true;
+					}
+					else if (!(i >= player2StartPoint && i < player2EndPoint) && ballX == 19) {
+						player1Points++;
+						system("CLS");
+						cout << "Player 1 wins" << endl;
+						for (size_t j = 0; j < 7; j++)
+						{
+							cout << "                    " << endl;
+						}
+						gameEnd = true;
+						system("pause>0");
+					}
+					//Increment the X value to move the ball horizontally
+					if (player1Shot) {
+						ballX++;
+					}
+					else if (player2Shot) {
+						ballX--;
+					}
+				}
+				else {
+					//Draw some empty line where nothing is
 					cout << "                    ";
-					break;
 				}
-
-				//If the ball hits player one the boolean is set to true to increment the value later
-				if (ballX == 1) {
-					player1Shot = true;
-					player2Shot = false;
+				if (i >= player2StartPoint && i < player2EndPoint) {
+					//Draw player 2 where the coordinates appear
+					cout << "|" << endl;
 				}
-				else if (!(i >= player1StartPoint && i < player1EndPoint) && ballX == 2) {
-					//If the ball is not hit by the player, the game ends
-					player2Points++;
-					system("CLS");
-					cout << "Player 2 wins" << endl;
-					for (size_t j = 0; j < 7; j++)
-					{
-						cout << "                    " << endl;
-					}
-					gameEnd = true;
-					system("pause>0");
-				}
-				if (ballX == 20) {
-					player1Shot = false;
-					player2Shot = true;
-				}
-				else if (!(i >= player2StartPoint && i < player2EndPoint) && ballX == 19) {
-					player1Points++;
-					system("CLS");
-					cout << "Player 1 wins" << endl;
-					for (size_t j = 0; j < 7; j++)
-					{
-						cout << "                    " << endl;
-					}
-					gameEnd = true;
-					system("pause>0");
-				}
-				//Increment the X value to move the ball horizontally
-				if (player1Shot) {
-					ballX++;
-				}
-				else if (player2Shot) {
-					ballX--;
+				else {
+					cout << endl;
 				}
 			}
-			else {
-			//Draw some empty line where nothing is
-				cout << "                    ";
+
+			//Setting the players booleans to false if the other player hit the ball
+
+			if (player2Shot) {
+				player1ShotTopEdge = false;
+				player1ShotBottomEdge = false;
 			}
-			if (i >= player2StartPoint && i < player2EndPoint) {
-				//Draw player 2 where the coordinates appear
-				cout << "|" << endl;
+			if (player1Shot) {
+				player2ShotTopEdge = false;
+				player2ShotBottomEdge = false;
 			}
-			else {
-				cout << endl;
+
+			/// /////////////////////////
+			//If a player hit the ball at the edge, it will also move vertically
+
+			if (player1Shot && ballX < player1StartPoint + 1) {
+				player1ShotTopEdge = true;
 			}
+			if (player1ShotTopEdge) {
+				ballY--;
+			}
+			if (player1Shot && ballX > player1EndPoint - 1) {
+				player1ShotBottomEdge = true;
+			}
+			if (player1ShotBottomEdge) {
+				ballY++;
+			}
+
+			/// /////////////////////////
+
+			if (player2Shot && ballX < player2StartPoint + 1) {
+				player2ShotTopEdge = true;
+			}
+			if (player2ShotTopEdge) {
+				ballY--;
+			}
+			if (player2Shot && ballX > player2EndPoint - 1) {
+				player2ShotBottomEdge = true;
+			}
+			if (player2ShotBottomEdge) {
+				ballY++;
+			}
+
+			/// /////////////////////////
+			//Save the balls if they want to go outside the playground
+
+			if (ballY <= 0) {
+				ballY++;
+			}
+			if (ballY == 20) {
+				ballY--;
+			}
+
+			//Just logging out the scoreboard
+			cout << "Player 1 score: " << player1Points << endl;
+			cout << "Player 2 score: " << player2Points << endl;
+			cout << endl;
+			cout << "By: Patrik Vertesi" << endl;
+
+			//Slow down the game loop
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
 
-		//Setting the players booleans to false if the other player hit the ball
-
-		if (player2Shot) {
-			player1ShotTopEdge = false;
-			player1ShotBottomEdge = false;
-		}
-		if (player1Shot) {
-			player2ShotTopEdge = false;
-			player2ShotBottomEdge = false;
-		}
-
-		/// /////////////////////////
-		//If a player hit the ball at the edge, it will also move vertically
-
-		if (player1Shot && ballX < player1StartPoint + 1) {
-			player1ShotTopEdge = true;
-		}
-		if (player1ShotTopEdge) {
-			ballY--;
-		}
-		if (player1Shot && ballX > player1EndPoint - 1) {
-			player1ShotBottomEdge = true;
-		}
-		if (player1ShotBottomEdge) {
-			ballY++;
-		}
-
-		/// /////////////////////////
-
-		if (player2Shot && ballX < player2StartPoint + 1) {
-			player2ShotTopEdge = true;
-		}
-		if (player2ShotTopEdge) {
-			ballY--;
-		}
-		if (player2Shot && ballX > player2EndPoint - 1) {
-			player2ShotBottomEdge = true;
-		}
-		if (player2ShotBottomEdge) {
-			ballY++;
-		}
-
-		/// /////////////////////////
-		//Save the balls if they want to go outside the playground
-
-		if (ballY <= 0) {
-			ballY++;
-		}
-		if (ballY == 20) {
-			ballY--;
-		}
-
-		//Just logging out the scoreboard
-		cout << "Player 1 score: " << player1Points << endl;
-		cout << "Player 2 score: " << player2Points << endl;
-
-		//Slow down the game loop
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		system("pause>0");
 	}
-
-	system("pause>0");
-		}
 }
